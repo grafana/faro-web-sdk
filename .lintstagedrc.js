3ee1f4e6fae39ed8ab00b2459b94ef9c07f22154
Bogdan Matei <bogdan.matei@grafana.com>
2021-09-30 16:18:45 +0300

    Update

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{js,jsx,ts,tsx,css,scss,md,yaml,yml,json}': ['prettier --write'],
   '*.{js,jsx,ts,tsx}': ['eslint', 'madge --extensions js,jsx,ts,tsx --circular --ts-config=tsconfig.json'],
-  '**/*.ts?(x)': () => 'tsc --noEmit',
+  '**/*.ts?(x)': 'tsc --noEmit',
 };
