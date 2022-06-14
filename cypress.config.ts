e602b279949d4145a828eda604e24ae55c17022a
Domas <domasx2@gmail.com>
2022-06-14 18:03:11 +0300

    remove unncessary stuff from cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,5 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:1234',
     video: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
