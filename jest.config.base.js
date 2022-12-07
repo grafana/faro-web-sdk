3b28e87f797b9399cd88f5dc05650ee3adf2700a
Bogdan Matei <bogdan@matei.dev>
2022-12-07 18:10:56 +0200

    Fix Jest tests

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,7 +1,18 @@
+const { join } = require('node:path');
+
 const jestBaseConfig = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
   roots: ['src'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': [
+      'ts-jest',
+      {
+        tsconfig: join(__dirname, 'tsconfig.spec.json'),
+        useESM: true
+      }
+    ]
+  }
 };
 
 module.exports = {
