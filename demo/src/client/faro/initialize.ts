9883ee8c3b4d8c90b829da52bb4641d51da462ac
marco.schaefer <marco.schaefer@grafana.com>
2023-04-13 15:09:07 +0200

    chore: cleanup initialize

--- demo/src/client/faro/initialize.ts
+++ demo/src/client/faro/initialize.ts
@@ -7,15 +7,14 @@ import {
   ReactRouterVersion,
 } from '@grafana/faro-react';
 import type { Faro } from '@grafana/faro-react';
-// import { BatchTransport } from '@grafana/faro-transport-batch';
 import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 
 import { env } from '../utils';
 
 export function initializeFaro(): Faro {
   const faro = coreInit({
-    // url: `http://localhost:${env.faro.portAppReceiver}/collect`,
-    // apiKey: env.faro.apiKey,
+    url: `http://localhost:${env.faro.portAppReceiver}/collect`,
+    apiKey: env.faro.apiKey,
     instrumentations: [
       ...getWebInstrumentations({
         captureConsole: true,
@@ -40,19 +39,6 @@ export function initializeFaro(): Faro {
       version: env.package.version,
       environment: env.mode.name,
     },
-    // transports: [
-    //   new BatchTransport(
-    //     new OtlpHttpTransport({
-    //       tracesURL: `http://localhost:${env.faro.portAppReceiver}/collect`,
-    //       logsURL: `http://localhost:${env.faro.portAppReceiver}/collect`,
-    //       apiKey: env.faro.apiKey,
-    //     }),
-    //     {
-    //       batchSendCount: 50,
-    //       batchSendTimeout: 500,
-    //     }
-    //   ),
-    // ],
   });
 
   faro.api.pushLog(['Faro was initialized']);
