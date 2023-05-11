e035a0f099208ff25d21a405583489e46754e658
marco.schaefer <marco.schaefer@grafana.com>
2023-05-11 16:53:57 +0200

    chore: remove PerfTimelineInstrumentation from demo.

--- demo/src/client/faro/initialize.ts
+++ demo/src/client/faro/initialize.ts
@@ -1,6 +1,5 @@
 import { createRoutesFromChildren, matchRoutes, Routes, useLocation, useNavigationType } from 'react-router-dom';
 
-import { PerformanceTimelineInstrumentation } from '@grafana/faro-instrumentation-performance-timeline';
 import {
   initializeFaro as coreInit,
   getWebInstrumentations,
@@ -32,9 +31,6 @@ export function initializeFaro(): Faro {
             useNavigationType,
           },
         },
-      }),
-      new PerformanceTimelineInstrumentation({
-        observeEntryTypes: [],
       }),
     ],
     session: (window as any).__PRELOADED_STATE__?.faro?.session,
