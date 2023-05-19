45e42cfe4ac4add5264fc22dc341e9f63fb5cb68
Sven Grossmann <Svennergr@gmail.com>
2023-05-19 10:35:17 +0200

    Core: Use `globalThis` rather than `window` (#195)

--- packages/core/src/globalObject/globalObject.ts
+++ packages/core/src/globalObject/globalObject.ts
@@ -1,10 +1,16 @@
 import type { Faro, internalGlobalObjectKey } from '../sdk';
 
-export type GlobalObject<T = typeof window | typeof global> = T & {
+export type GlobalObject<T = typeof globalThis | typeof global | typeof self> = T & {
   [label: string]: Faro;
 
   [internalGlobalObjectKey]: Faro;
 };
 
 // This does not uses isUndefined method because it will throw an error in non-browser environments
-export const globalObject = (typeof window === 'undefined' ? global : window) as GlobalObject;
+export const globalObject = (typeof globalThis !== 'undefined'
+  ? globalThis
+  : typeof global !== 'undefined'
+  ? global
+  : typeof self !== 'undefined'
+  ? self
+  : undefined) as unknown as GlobalObject;
