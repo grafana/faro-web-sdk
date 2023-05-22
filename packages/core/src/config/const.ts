cbbf39002b25af3e53134b98053a009b6773f4b1
Kostas Pelelis <kpelelisos@gmail.com>
2023-05-22 17:18:10 +0300

    Add default config for batching
    Co-Authored-By: Marco Schaefer <47627413+codecapitano@users.noreply.github.com>

--- packages/core/src/config/const.ts
+++ packages/core/src/config/const.ts
@@ -1,1 +1,7 @@
 export const defaultGlobalObjectKey = 'faro';
+
+export const defaultBatchingConfig = {
+  enabled: true,
+  sendTimeout: 250,
+  itemLimit: 50,
+};
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -34,7 +34,7 @@ export type { GlobalObject } from './globalObject';
 
 export { initializeFaro } from './initialize';
 
-export { defaultGlobalObjectKey } from './config';
+export { defaultBatchingConfig, defaultGlobalObjectKey } from './config';
 export type { Config, Patterns } from './config';
 
 export { BaseInstrumentation } from './instrumentations';
