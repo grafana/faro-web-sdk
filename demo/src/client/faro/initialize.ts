55cedb7c58007108a2b81ea62a01daad3dded863
Elliot Kirk <elliot.kirk@grafana.com>
2023-06-08 14:12:13 -0700

    Disable batching

--- demo/src/client/faro/initialize.ts
+++ demo/src/client/faro/initialize.ts
@@ -35,7 +35,7 @@ export function initializeFaro(): Faro {
     ],
     session: (window as any).__PRELOADED_STATE__?.faro?.session,
     batching: {
-      sendTimeout: 1,
+      enabled: false,
     },
     app: {
       name: env.client.packageName,
