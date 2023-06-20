d44afdcf7a904c7841bbf4e515c92f497787f64a
Cedric Ziel <cedric@cedric-ziel.com>
2023-06-20 10:33:08 +0200

    Remove OTel user-interaction instrumentation from the default set
    This instrumentation is very chatty and should only be enabled when
    needed. It is not needed for the default set of instrumentations.

--- packages/web-tracing/src/getDefaultOTELInstrumentations.ts
+++ packages/web-tracing/src/getDefaultOTELInstrumentations.ts
@@ -1,7 +1,6 @@
 import type { InstrumentationOption } from '@opentelemetry/instrumentation';
 import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
 import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch';
-import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction';
 import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request';
 
 import type { MatchUrlDefinitions } from './types';
@@ -23,6 +22,5 @@ export function getDefaultOTELInstrumentations(
     new DocumentLoadInstrumentation(),
     new FetchInstrumentation(options),
     new XMLHttpRequestInstrumentation(options),
-    new UserInteractionInstrumentation(),
   ];
 }
