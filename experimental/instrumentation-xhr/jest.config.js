b7cf74aeb1ea9bdc44874aaa7958740d78edc024
Elliot Kirk <elliot.kirk@grafana.com>
2023-08-03 18:48:20 -0700

    Fixing find + replace issue in jest config

--- experimental/instrumentation-xhr/jest.config.js
+++ experimental/instrumentation-xhr/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   moduleNameMapper: {
     '^@remix-run/router$': '<rootDir>/index.ts',
     '^@remix-run/web-blob$': require.resolve('@remix-run/web-blob'),
-    '^@remix-run/web-xhr$': require.resolve('@remix-run/web-xhr'),
+    '^@remix-run/web-fetch$': require.resolve('@remix-run/web-fetch'),
     '^@remix-run/web-form-data$': require.resolve('@remix-run/web-form-data'),
     '^@remix-run/web-stream$': require.resolve('@remix-run/web-stream'),
     '^@web3-storage/multipart-parser$': require.resolve('@web3-storage/multipart-parser'),
