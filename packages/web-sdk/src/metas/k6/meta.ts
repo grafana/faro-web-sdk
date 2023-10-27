34e4c598a4c457cf49c8f95a0f0320a0f40a26c4
Elliot Kirk <elliot.kirk@grafana.com>
2023-10-27 13:45:16 -0700

    adding isK6Browser to k6 meta payload (#361)

--- packages/web-sdk/src/metas/k6/meta.ts
+++ packages/web-sdk/src/metas/k6/meta.ts
@@ -2,6 +2,8 @@ import type { Meta, MetaItem } from '@grafana/faro-core';
 
 export const k6Meta: MetaItem<Pick<Meta, 'k6'>> = () => {
   return {
-    k6: {},
+    k6: {
+      isK6Browser: true,
+    },
   };
 };
