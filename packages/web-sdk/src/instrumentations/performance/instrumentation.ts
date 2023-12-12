a33f03c49f76f199e17385e9d3b394882f6508f4
marco.schaefer <marco.schaefer@grafana.com>
2023-12-12 11:13:39 +0100

    check for performance observer support

--- packages/web-sdk/src/instrumentations/performance/instrumentation.ts
+++ packages/web-sdk/src/instrumentations/performance/instrumentation.ts
@@ -2,13 +2,17 @@ import { BaseInstrumentation, VERSION } from '@grafana/faro-core';
 
 import { getNavigationTimings } from './navigation';
 import { observeResourceTimings } from './resource';
-import { onDocumentReady } from './util';
+import { onDocumentReady, performanceObserverSupported } from './util';
 
 export class PerformanceInstrumentation extends BaseInstrumentation {
   readonly name = '@grafana/faro-web-sdk:instrumentation-performance';
   readonly version = VERSION;
 
   initialize() {
+    if (!performanceObserverSupported()) {
+      return;
+    }
+
     onDocumentReady(() => {
       const faroNavigationTiming = getNavigationTimings(this.api.pushEvent, this.getIgnoreUrls());
       observeResourceTimings(faroNavigationTiming as any, this.api.pushEvent, this.getIgnoreUrls());
