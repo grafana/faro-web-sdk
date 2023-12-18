88e0cc59e23dd22f4678f30babc9d715cf9bb1c9
marco.schaefer <marco.schaefer@grafana.com>
2023-12-18 17:56:10 +0100

    add toString converter

--- packages/web-sdk/src/instrumentations/performance/performanceUtils.ts
+++ packages/web-sdk/src/instrumentations/performance/performanceUtils.ts
@@ -23,19 +23,25 @@ export function onDocumentReady(handleReady: () => void) {
 export function calculateFaroResourceTimings(resourceEntryRaw: PerformanceResourceTiming): FaroResourceTiming {
   return {
     name: resourceEntryRaw.name,
-    tcpHandshakeTime: `${resourceEntryRaw.connectEnd - resourceEntryRaw.connectStart}`,
-    dnsLookupTime: `${resourceEntryRaw.domainLookupEnd - resourceEntryRaw.domainLookupStart}`,
-    tlsNegotiationTime: `${resourceEntryRaw.requestStart - resourceEntryRaw.secureConnectionStart}`,
-    redirectLookupTime: `${resourceEntryRaw.redirectEnd - resourceEntryRaw.redirectStart}`,
-    requestTime: `${resourceEntryRaw.responseStart - resourceEntryRaw.requestStart}`,
-    fetchTime: `${resourceEntryRaw.responseEnd - resourceEntryRaw.fetchStart}`,
-    serviceWorkerProcessingTime: `${resourceEntryRaw.fetchStart - resourceEntryRaw.workerStart}`,
-    decodedBodySize: `${resourceEntryRaw.encodedBodySize}`,
-    unCompressedBodySize: `${resourceEntryRaw.decodedBodySize}`,
-    isCacheHit: `${resourceEntryRaw.transferSize === 0 && resourceEntryRaw.decodedBodySize > 0}`,
+    tcpHandshakeTime: toFaroPerformanceTimingString(resourceEntryRaw.connectEnd - resourceEntryRaw.connectStart),
+    dnsLookupTime: toFaroPerformanceTimingString(resourceEntryRaw.domainLookupEnd - resourceEntryRaw.domainLookupStart),
+    tlsNegotiationTime: toFaroPerformanceTimingString(
+      resourceEntryRaw.requestStart - resourceEntryRaw.secureConnectionStart
+    ),
+    redirectLookupTime: toFaroPerformanceTimingString(resourceEntryRaw.redirectEnd - resourceEntryRaw.redirectStart),
+    requestTime: toFaroPerformanceTimingString(resourceEntryRaw.responseStart - resourceEntryRaw.requestStart),
+    fetchTime: toFaroPerformanceTimingString(resourceEntryRaw.responseEnd - resourceEntryRaw.fetchStart),
+    serviceWorkerProcessingTime: toFaroPerformanceTimingString(
+      resourceEntryRaw.fetchStart - resourceEntryRaw.workerStart
+    ),
+    decodedBodySize: toFaroPerformanceTimingString(resourceEntryRaw.encodedBodySize),
+    unCompressedBodySize: toFaroPerformanceTimingString(resourceEntryRaw.decodedBodySize),
+    isCacheHit: toFaroPerformanceTimingString(
+      resourceEntryRaw.transferSize === 0 && resourceEntryRaw.decodedBodySize > 0
+    ),
 
     // @ts-expect-error the renderBlocking property is not available in all browsers
-    renderBlockingStatus: resourceEntryRaw.renderBlockingStatus ?? 'unknown',
+    renderBlockingStatus: toFaroPerformanceTimingString(resourceEntryRaw.renderBlockingStatus),
 
     protocol: resourceEntryRaw.nextHopProtocol,
     initiatorType: resourceEntryRaw.initiatorType,
@@ -45,15 +51,33 @@ export function calculateFaroResourceTimings(resourceEntryRaw: PerformanceResour
 
 export function calculateFaroNavigationTimings(navigationEntryRaw: PerformanceNavigationTiming): FaroNavigationTiming {
   return {
-    totalNavigationTime: `${navigationEntryRaw.duration}`,
+    totalNavigationTime: toFaroPerformanceTimingString(navigationEntryRaw.duration),
     visibilityState: document.visibilityState,
-    pagLoadTime: `${navigationEntryRaw.domContentLoadedEventStart - navigationEntryRaw.fetchStart}`,
-    documentProcessingDuration: `${navigationEntryRaw.loadEventEnd - navigationEntryRaw.responseEnd}`,
-    domLoadTime: `${navigationEntryRaw.loadEventEnd - navigationEntryRaw.domContentLoadedEventEnd}`,
-    scriptProcessingDuration: `${
+    pagLoadTime: toFaroPerformanceTimingString(
+      navigationEntryRaw.domContentLoadedEventStart - navigationEntryRaw.fetchStart
+    ),
+    documentProcessingDuration: toFaroPerformanceTimingString(
+      navigationEntryRaw.loadEventEnd - navigationEntryRaw.responseEnd
+    ),
+    domLoadTime: toFaroPerformanceTimingString(
+      navigationEntryRaw.loadEventEnd - navigationEntryRaw.domContentLoadedEventEnd
+    ),
+    scriptProcessingDuration: toFaroPerformanceTimingString(
       navigationEntryRaw.domContentLoadedEventEnd - navigationEntryRaw.domContentLoadedEventStart
-    }`,
-    ttfb: `${navigationEntryRaw.responseStart - navigationEntryRaw.fetchStart}`,
+    ),
+    ttfb: toFaroPerformanceTimingString(navigationEntryRaw.responseStart - navigationEntryRaw.fetchStart),
     type: navigationEntryRaw.type,
   };
+}
+
+function toFaroPerformanceTimingString(v: unknown): string {
+  if (v == null) {
+    return 'unknown';
+  }
+
+  if (typeof v === 'number') {
+    return Math.ceil(v).toString();
+  }
+
+  return v.toString();
 }
