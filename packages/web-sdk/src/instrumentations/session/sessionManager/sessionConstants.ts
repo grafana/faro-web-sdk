550301a5960ed4be7f95b2dff956bab83ff0d23e
Marco Schaefer <47627413+codecapitano@users.noreply.github.com>
2023-12-19 12:59:12 +0100

    chore: use reverse domain name as web-storage item prefix (#432)

--- packages/web-sdk/src/instrumentations/session/sessionManager/sessionConstants.ts
+++ packages/web-sdk/src/instrumentations/session/sessionManager/sessionConstants.ts
@@ -1,6 +1,6 @@
 import type { Config } from '@grafana/faro-core';
 
-export const STORAGE_KEY = '__FARO_SESSION__';
+export const STORAGE_KEY = 'com.grafana.faro.session';
 export const SESSION_EXPIRATION_TIME = 4 * 60 * 60 * 1000; // hrs
 export const SESSION_INACTIVITY_TIME = 15 * 60 * 1000; // minutes
 export const STORAGE_UPDATE_DELAY = 1 * 1000; // seconds
