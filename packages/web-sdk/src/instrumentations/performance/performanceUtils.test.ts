c6ae01d6aee77ad11dac306ab197eeb9bd96f86e
Cedric Ziel <cedric@cedric-ziel.com>
2024-06-18 15:00:51 +0200

    chore: Add negative tests

--- packages/web-sdk/src/instrumentations/performance/performanceUtils.test.ts
+++ packages/web-sdk/src/instrumentations/performance/performanceUtils.test.ts
@@ -1,4 +1,9 @@
-import { createFaroNavigationTiming, createFaroResourceTiming, includePerformanceEntry } from './performanceUtils';
+import {
+  createFaroNavigationTiming,
+  createFaroResourceTiming,
+  getSpanContextFromServerTiming,
+  includePerformanceEntry
+} from './performanceUtils';
 import { performanceNavigationEntry, performanceResourceEntry } from './performanceUtilsTestData';
 import type { FaroNavigationTiming, FaroResourceTiming } from './types';
 
@@ -158,4 +163,43 @@ describe('performanceUtils', () => {
     });
     expect(matchByMultiValues1).toBe(false);
   });
+
+  it('Can extract a span context if server returns a traceId and spanId', () => {
+    const serverTimings: PerformanceServerTiming[] = [
+      {
+        name: 'traceparent',
+        description: '00-1234-5678-01',
+        duration: 0,
+        toJSON(): any {
+        }
+      },
+    ];
+
+    const spanContext = getSpanContextFromServerTiming(serverTimings);
+    expect(spanContext).toStrictEqual({ traceId: '1234', spanId: '5678' });
+  });
+
+  it('Ignores incoming traceparent server-timings if they are not conformant to w3c trace-context', () => {
+    const serverTimings: PerformanceServerTiming[] = [
+      {
+        name: 'traceparent',
+        description: '00-1234-5678-01-02',
+        duration: 0,
+        toJSON(): any {
+        }
+      },
+    ];
+
+    const spanContext = getSpanContextFromServerTiming(serverTimings);
+    expect(spanContext).toBeUndefined();
+
+    const emptyServerTimings: PerformanceServerTiming[] = [];
+    const spanContextEmpty = getSpanContextFromServerTiming(emptyServerTimings);
+    expect(spanContextEmpty).toBeUndefined();
+
+    const undefinedServerTimings = undefined;
+    const spanContextUndefined = getSpanContextFromServerTiming(undefinedServerTimings);
+    expect(spanContextUndefined).toBeUndefined();
+  });
 });
+
