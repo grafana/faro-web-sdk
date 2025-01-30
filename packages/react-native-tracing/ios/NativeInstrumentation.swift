8438b12f8536afbd0ad399fa82f6e7b4866a6dbc
Lucas Bento <lucas@bitvavo.com>
2025-01-30 15:43:13 +0100

    refactor: add missing `do-catch`

--- packages/react-native-tracing/ios/NativeInstrumentation.swift
+++ packages/react-native-tracing/ios/NativeInstrumentation.swift
@@ -35,31 +35,37 @@ public class NativeInstrumentation: NSObject, RCTBridgeModule {
     
     @objc
     public func getStartupTime(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
-        var mib = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
-        var size = MemoryLayout<kinfo_proc>.size
-        var kp = kinfo_proc()
-        
-        let result = mib.withUnsafeMutableBytes { mibBytes in
-            withUnsafeMutablePointer(to: &size) { sizeBytes in
-                withUnsafeMutablePointer(to: &kp) { kpBytes in
-                    sysctl(mibBytes.baseAddress?.assumingMemoryBound(to: Int32.self), 4,
-                           kpBytes,
-                           sizeBytes,
-                           nil, 0)
+        do {
+            var mib = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+            var size = MemoryLayout<kinfo_proc>.size
+            var kp = kinfo_proc()
+            
+            let result = mib.withUnsafeMutableBytes { mibBytes in
+                withUnsafeMutablePointer(to: &size) { sizeBytes in
+                    withUnsafeMutablePointer(to: &kp) { kpBytes in
+                        sysctl(mibBytes.baseAddress?.assumingMemoryBound(to: Int32.self), 4,
+                               kpBytes,
+                               sizeBytes,
+                               nil, 0)
+                    }
                 }
             }
+            
+            let startTimeMs: Int64
+            if result == 0 {
+                let startTime = kp.kp_proc.p_un.__p_starttime
+                startTimeMs = Int64(startTime.tv_sec) * 1000 + Int64(startTime.tv_usec) / 1000
+            } else {
+                throw NSError(domain: "NativeInstrumentation", 
+                            code: Int(result), 
+                            userInfo: [NSLocalizedDescriptionKey: "Failed to get process info"])
+            }
+            
+            let response = ["startupTime": startTimeMs]
+            resolve(response)
+        } catch {
+            reject("STARTUP_TIME_ERROR", "Failed to get startup time: \(error.localizedDescription)", error)
         }
-        
-        let startTimeMs: Int64
-        if result == 0 {
-            let startTime = kp.kp_proc.p_un.__p_starttime
-            startTimeMs = Int64(startTime.tv_sec) * 1000 + Int64(startTime.tv_usec) / 1000
-        } else {
-            startTimeMs = Int64(Date().timeIntervalSince1970 * 1000)
-        }
-        
-        let response = ["startupTime": startTimeMs]
-        resolve(response)
     }
     
     @objc
