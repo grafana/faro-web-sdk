a59044fd0aa5e1554d77262b4a9d5a3076a41740
marco.schaefer <marco.schaefer@grafana.com>
2025-02-14 17:45:08 +0100

    fix test setup

--- experimental/instrumentation-performance-timeline/jest.config.js
+++ experimental/instrumentation-performance-timeline/jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   ...jestBaseConfig,
   roots: ['experimental/instrumentation-performance-timeline/src'],
   testEnvironment: 'jsdom',
+  setupFiles: ['<rootDir>/packages/instrumentation-performance-timeline/setup.jest.ts'],
 };
