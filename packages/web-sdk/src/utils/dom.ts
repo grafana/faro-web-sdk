34777f72aa6b6c5d24df4ec0a79b9613ee639e46
marco.schaefer <marco.schaefer@grafana.com>
2025-03-13 10:40:09 +0100

    define action names

--- packages/web-sdk/src/utils/dom.ts
+++
@@ -1,7 +0,0 @@
-export function isClickableElement(target: HTMLElement): boolean {
-  return (
-    ['A', 'BUTTON'].includes(target.tagName) ||
-    target.hasAttribute('onclick') ||
-    target.getAttribute('role') === 'button'
-  );
-}
