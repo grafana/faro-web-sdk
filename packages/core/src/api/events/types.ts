c1bcdbebcb83674a3e3f8ee53172b914666607c9
marco.schaefer <marco.schaefer@grafana.com>
2025-03-14 14:55:56 +0100

    update

--- packages/core/src/api/events/types.ts
+++ packages/core/src/api/events/types.ts
@@ -20,6 +20,16 @@ export interface PushEventOptions {
   skipDedupe?: boolean;
   spanContext?: Pick<SpanContext, 'traceId' | 'spanId'>;
   timestampOverwriteMs?: number;
+
+  /**
+   * Allows manual parsing of the payload before adding it to the internal buffer.
+   *
+   * @param payload - The event payload to be parsed.
+   * @returns The parsed event payload.
+   *
+   * @remarks This should be used sparingly and only in special cases where custom payload processing cannot be deferred to the before-send hook.
+   */
+  customPayloadParser?: (payload: EventEvent) => EventEvent;
 }
 
 export interface EventsAPI {
