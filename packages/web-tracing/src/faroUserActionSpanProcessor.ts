a09f724437a2ac1476873d9f1d8b9c9721ff8a03
marco.schaefer <marco.schaefer@grafana.com>
2025-03-14 12:49:31 +0100

    fixes and cleanup

--- packages/web-tracing/src/faroUserActionSpanProcessor.ts
+++ packages/web-tracing/src/faroUserActionSpanProcessor.ts
@@ -1,9 +1,9 @@
 import { type Context, SpanKind } from '@opentelemetry/api';
 import type { ReadableSpan, Span, SpanProcessor } from '@opentelemetry/sdk-trace-web';
 
-import { apiMessageBus, type ApiMessageBusMessage } from '@grafana/faro-web-sdk';
+import { apiMessageBus, type UserActionStartMessage } from '@grafana/faro-web-sdk';
 
-let message: ApiMessageBusMessage | undefined;
+let message: UserActionStartMessage | undefined;
 
 apiMessageBus.subscribe((msg) => {
   if (msg.type === 'user-action-start') {
@@ -25,6 +25,9 @@ export class FaroUserActionSpanProcessor implements SpanProcessor {
 
   onStart(span: Span, parentContext: Context): void {
     if (span.kind === SpanKind.CLIENT) {
+      // If the span is created when teh message object is available it is created during the user action timeout span.
+      // In this case we can add the user action name and parentId to the span attributes.
+      // If the span is created after the user action timeout span, the message object will be undefined which means the action has been cancelled or is ended.
       if (message) {
         span.attributes['faro.action.user.name'] = message?.name;
         span.attributes['faro.action.user.parentId'] = message?.parentId;
