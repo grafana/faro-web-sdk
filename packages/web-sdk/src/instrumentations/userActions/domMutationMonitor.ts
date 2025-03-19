174984e2b8b2b2a14405b42a8104daf0c1430dfd
marco.schaefer <marco.schaefer@grafana.com>
2025-03-19 12:41:44 +0100

    update and test domMutationMonitor

--- packages/web-sdk/src/instrumentations/userActions/domMutationMonitor.ts
+++ packages/web-sdk/src/instrumentations/userActions/domMutationMonitor.ts
@@ -1,9 +1,10 @@
 import { Observable } from '@grafana/faro-core';
 
 import { MESSAGE_TYPE_DOM_MUTATION } from './const';
+import type { DomMutationMessage } from './types';
 
 export function monitorDomMutations(): Observable {
-  const observable = new Observable();
+  const observable = new Observable<DomMutationMessage>();
 
   const observer = new MutationObserver((_mutationsList, _observer) => {
     observable.notify({ type: MESSAGE_TYPE_DOM_MUTATION });
