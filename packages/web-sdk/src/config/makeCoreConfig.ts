4e750512e83a970e5d87b099f7e0411354258d0d
marco.schaefer <marco.schaefer@grafana.com>
2025-03-20 13:28:49 +0100

    config test and updates

--- packages/web-sdk/src/config/makeCoreConfig.ts
+++ packages/web-sdk/src/config/makeCoreConfig.ts
@@ -109,7 +109,7 @@ export function makeCoreConfig(browserConfig: BrowserConfig): Config {
     consoleInstrumentation,
     webVitalsInstrumentation,
     trackUserActions,
-    trackUserActionsDataAttributeName: parseActionAttributeName(trackUserActionsDataAttributeName),
+    trackUserActionsDataAttributeName,
   };
 }
 
@@ -162,17 +162,4 @@ function crateSessionMeta({
       overrides,
     },
   };
-}
-
-/**
- * Parses the action attribute name by removing the 'data-' prefix and converting
- * the remaining string to camelCase.
- *
- * This is needed because the browser will remove the 'data-' prefix and the dashes from
- * data attributes and make then camelCase.
- */
-function parseActionAttributeName(userActionDataAttribute: string) {
-  const withoutData = userActionDataAttribute.split('data-')[1];
-  const withUpperCase = withoutData?.replace(/-(.)/g, (_, char) => char.toUpperCase());
-  return withUpperCase?.replace(/-/g, '');
 }
