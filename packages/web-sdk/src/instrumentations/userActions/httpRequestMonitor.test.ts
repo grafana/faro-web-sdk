9e7b048259c6a6f036d98785e304dc41db683543
marco.schaefer <marco.schaefer@grafana.com>
2025-03-20 17:58:28 +0100

    update tests

--- packages/web-sdk/src/instrumentations/userActions/httpRequestMonitor.test.ts
+++ packages/web-sdk/src/instrumentations/userActions/httpRequestMonitor.test.ts
@@ -1,44 +1,66 @@
 import { initializeFaro } from '@grafana/faro-core';
 import { mockConfig } from '@grafana/faro-core/src/testUtils';
 
-import { MESSAGE_TYPE_HTTP_REQUEST_START } from './const';
+import { MESSAGE_TYPE_HTTP_REQUEST_END, MESSAGE_TYPE_HTTP_REQUEST_START } from './const';
 import { monitorHttpRequests } from './httpRequestMonitor';
 
 describe('monitorHttpRequests', () => {
-  it('Monitors xhr requests and sends a message if request are pending', () => {
+  beforeEach(() => {});
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('Monitors xhr requests and sends a message if request are pending', async () => {
     const observable = monitorHttpRequests();
-    const mockNotify = jest.fn();
-    jest.spyOn(observable, 'notify').mockImplementationOnce(mockNotify);
+    const mockSubscribe = jest.fn();
+    observable.subscribe(mockSubscribe);
 
     initializeFaro(mockConfig());
 
     const xhr = new XMLHttpRequest();
 
     xhr.open('GET', 'https://www.grafana.com');
-    xhr.send();
 
-    expect(mockNotify).toHaveBeenCalledTimes(1);
-    expect(mockNotify).toHaveBeenNthCalledWith(1, { type: MESSAGE_TYPE_HTTP_REQUEST_START });
+    await new Promise((resolve) => {
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          resolve({});
+        }
+      };
+      xhr.send();
+    });
+
+    expect(mockSubscribe).toHaveBeenCalledTimes(2);
+    expect(mockSubscribe).toHaveBeenNthCalledWith(1, { type: MESSAGE_TYPE_HTTP_REQUEST_START, pending: 1 });
+    expect(mockSubscribe).toHaveBeenNthCalledWith(2, { type: MESSAGE_TYPE_HTTP_REQUEST_END, pending: 0 });
   });
 
-  it('Monitors fetch requests and sends a message if request are pending', () => {
+  it('Monitors fetch requests and sends a message if request are pending', async () => {
+    const originalFetch = global.fetch;
+
     global.fetch = jest.fn(() =>
       Promise.resolve({
-        json: () => Promise.resolve({ data: 'mocked data' }),
-        ok: true,
-        status: 200,
+        json: () => Promise.resolve({ key: 'value' }),
       })
     ) as any;
 
     const observable = monitorHttpRequests();
-    const mockNotify = jest.fn();
-    jest.spyOn(observable, 'notify').mockImplementationOnce(mockNotify);
+    const mockSubscribe = jest.fn();
+    observable.subscribe(mockSubscribe);
 
     initializeFaro(mockConfig());
 
-    fetch('https://www.grafana.com');
+    await fetch('https://www.grafana.com');
 
-    expect(mockNotify).toHaveBeenCalledTimes(1);
-    expect(mockNotify).toHaveBeenNthCalledWith(1, { type: MESSAGE_TYPE_HTTP_REQUEST_START });
+    expect(mockSubscribe).toHaveBeenCalledTimes(2);
+    expect(mockSubscribe).toHaveBeenNthCalledWith(1, { type: MESSAGE_TYPE_HTTP_REQUEST_START, pending: 1 });
+    expect(mockSubscribe).toHaveBeenNthCalledWith(2, { type: MESSAGE_TYPE_HTTP_REQUEST_END, pending: 0 });
+
+    global.fetch = originalFetch;
   });
 });
