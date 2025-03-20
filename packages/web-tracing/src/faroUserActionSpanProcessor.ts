e30a35adf6cbfb36d48d9a4dce6b581d9b5bbf2e
marco.schaefer <marco.schaefer@grafana.com>
2025-03-20 14:14:13 +0100

    test user action span processor

--- packages/web-tracing/src/faroUserActionSpanProcessor.ts
+++ packages/web-tracing/src/faroUserActionSpanProcessor.ts
@@ -3,21 +3,23 @@ import type { ReadableSpan, Span, SpanProcessor } from '@opentelemetry/sdk-trace
 
 import { apiMessageBus, type UserActionStartMessage } from '@grafana/faro-web-sdk';
 
-let message: UserActionStartMessage | undefined;
-
-apiMessageBus.subscribe((msg) => {
-  if (msg.type === 'user-action-start') {
-    message = msg;
-    return;
-  }
-
-  if (['user-action-end', 'user-action-cancel'].includes(msg.type)) {
-    message = undefined;
-  }
-});
-
 export class FaroUserActionSpanProcessor implements SpanProcessor {
-  constructor(private processor: SpanProcessor) {}
+  message: UserActionStartMessage | undefined;
+
+  constructor(private processor: SpanProcessor) {
+    apiMessageBus.subscribe((msg) => {
+      console.log(msg);
+      if (msg.type === 'user-action-start') {
+        this.message = msg;
+        return;
+      }
+
+      if (['user-action-end', 'user-action-cancel'].includes(msg.type)) {
+        console.log('clearing message');
+        this.message = undefined;
+      }
+    });
+  }
 
   forceFlush(): Promise<void> {
     return this.processor.forceFlush();
@@ -25,12 +27,12 @@ export class FaroUserActionSpanProcessor implements SpanProcessor {
 
   onStart(span: Span, parentContext: Context): void {
     if (span.kind === SpanKind.CLIENT) {
-      // If the span is created when teh message object is available it is created during the user action timeout span.
+      // If the span is created when the message object is available it is created before the user action timeout has been reached so it belongs to the user-action.
       // In this case we can add the user action name and parentId to the span attributes.
       // If the span is created after the user action timeout span, the message object will be undefined which means the action has been cancelled or is ended.
-      if (message) {
-        span.attributes['faro.action.user.name'] = message?.name;
-        span.attributes['faro.action.user.parentId'] = message?.parentId;
+      if (this.message) {
+        span.attributes['faro.action.user.name'] = this.message?.name;
+        span.attributes['faro.action.user.parentId'] = this.message?.parentId;
       }
     }
 
