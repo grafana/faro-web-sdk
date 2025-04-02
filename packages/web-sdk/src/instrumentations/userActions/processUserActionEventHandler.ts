ac89f54359ef08f8a1ad8d917d4f9f68d6ca8ee1
marco.schaefer <marco.schaefer@grafana.com>
2025-04-02 12:30:29 +0200

    wip

--- packages/web-sdk/src/instrumentations/userActions/processUserActionEventHandler.ts
+++ packages/web-sdk/src/instrumentations/userActions/processUserActionEventHandler.ts
@@ -19,7 +19,7 @@ import { convertDataAttributeName } from './util';
 export function getUserEventHandler(faro: Faro) {
   const { api, config } = faro;
 
-  const httpMonitor = monitorHttpRequests();
+  const { observable: httpMonitor, resetCounters: resetHttpCounters } = monitorHttpRequests();
   const domMutationsMonitor = monitorDomMutations();
   const performanceEntriesMonitor = monitorPerformanceEntries();
 
@@ -38,6 +38,8 @@ export function getUserEventHandler(faro: Faro) {
     if (actionRunning || userActionName == null) {
       return;
     }
+
+    resetHttpCounters();
 
     actionRunning = true;
 
@@ -66,7 +68,9 @@ export function getUserEventHandler(faro: Faro) {
 
     allMonitorsSub = allMonitorsObserver
       .takeWhile(() => actionRunning)
-      .subscribe(() => {
+      .subscribe((msg) => {
+        console.log('msg', msg);
+
         // A http request, a DOM mutation or a performance entry happened so we have a follow up activity and start the timeout again
         // If timeout is triggered the user action is done and we send respective messages and events
         timeoutId = startTimeout(timeoutId, () => {
