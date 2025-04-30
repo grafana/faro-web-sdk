55e1e7b7cc37196d4a05409d738bbadbf4de247c
marco.schaefer <marco.schaefer@grafana.com>
2025-04-30 16:18:21 +0200

    update commitlint config

---
+++ .github/linters/commitlint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'subject-case': [0, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', ['lower-case', 'camel-case']],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'deps'],
+    ],
+  },
+};
