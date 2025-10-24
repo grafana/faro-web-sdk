491995068876f82ef77951eb60cf768c7c3ef693
Martin Kuba <martin@martinkuba.com>
2025-10-24 15:14:52 -0700

    updated events tracker with a newer version

--- packages/web-sdk/src/instrumentations/softNavigation/instrumentation.ts
+++ packages/web-sdk/src/instrumentations/softNavigation/instrumentation.ts
@@ -1,12 +1,14 @@
 import { BaseInstrumentation, VERSION, faro, Observable } from '@grafana/faro-core';
-import { monitorHttpRequests } from '../userActions/httpRequestMonitor';
-import { monitorDomMutations } from '../userActions/domMutationMonitor';
-import { monitorUrlChanges } from './urlChangeMonitor';
-import EventsTracker from './eventsTracker';
-import { monitorInteractions } from './interactionMonitor';
+import { ActivityWindowTracker } from '../_internal/activityWindowTracker';
+import { monitorHttpRequests } from '../_internal/monitors/httpRequestMonitor';
+import { monitorDomMutations } from '../_internal/monitors/domMutationMonitor';
+import { monitorUrlChanges } from '../_internal/monitors/urlChangeMonitor';
+import { monitorInteractions } from '../_internal/monitors/interactionMonitor';
+import { isRequestStartMessage } from '../_internal/activityWindowTracker';
+import { isRequestEndMessage } from './eventsTracker';
 
 export class SoftNavigationInstrumentation extends BaseInstrumentation {
-  readonly name = '@grafana/faro-web-sdk:instrumentation-soft-navigation';
+  readonly name = '@grafana/faro-web-sdk:instrumentation-navigation';
   readonly version = VERSION;
 
   override initialize(): void {
@@ -15,24 +17,29 @@ export class SoftNavigationInstrumentation extends BaseInstrumentation {
     const urlMonitor = monitorUrlChanges();
     const interactionMonitor = monitorInteractions(['pointerdown', 'keydown']);
 
-    const tracker = new EventsTracker(interactionMonitor,
-      new Observable().merge(httpMonitor, domMutationsMonitor, urlMonitor)
+    const activityWindowTracker = new ActivityWindowTracker(
+      new Observable().merge(httpMonitor, domMutationsMonitor, urlMonitor),
+      {
+        followUpMs: 100,
+        haltMs: 10 * 1000,
+        isBlockingStart: (msg) => (isRequestStartMessage(msg) ? msg.request.requestId : undefined),
+        isBlockingEnd: (msg) => (isRequestEndMessage(msg) ? msg.request.requestId : undefined),
+      }
     );
 
-    tracker
-      .filter((msg) => {
-        return msg.message == 'tracking-ended';
-      })  
-      .subscribe((msg) => {
-        if (msg.events?.some((e: any) => e.type === 'url-change') && msg.events?.some((e: any) => e.type === 'dom-mutation'))
-        {
-          faro.api.pushEvent('soft-navigation', {
-            from: msg.events?.find((e: any) => e.type === 'url-change')?.from,
-            to: msg.events?.find((e: any) => e.type === 'url-change')?.to,
-            trigger: msg.events?.find((e: any) => e.type === 'url-change')?.trigger,
-            duration: msg.duration
-          });
-        }
-      });
+    activityWindowTracker.subscribe((msg) => {
+      if (msg.message == 'tracking-ended') {
+        faro.api.pushEvent('navigation', {
+          from: msg.events?.find((e: any) => e.type === 'url-change')?.from,
+          to: msg.events?.find((e: any) => e.type === 'url-change')?.to,
+          trigger: msg.events?.find((e: any) => e.type === 'url-change')?.trigger,
+          duration: msg.duration
+        });
+      }
+    });
+
+    interactionMonitor.subscribe(() => {
+      activityWindowTracker.startTracking();
+    });
   }
 }
