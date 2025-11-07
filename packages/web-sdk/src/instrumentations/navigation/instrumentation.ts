a36efe980ac626e9d230d82e2115061355a3336b
Martin Kuba <martin@martinkuba.com>
2025-11-07 11:09:21 -0800

    removed duplication and made it more explicit that sameDocument is a boolean attribute

--- packages/web-sdk/src/instrumentations/navigation/instrumentation.ts
+++ packages/web-sdk/src/instrumentations/navigation/instrumentation.ts
@@ -35,10 +35,11 @@ export class NavigationInstrumentation extends BaseInstrumentation {
           msg.events?.some((e: any) => e.type === 'url-change') &&
           msg.events?.some((e: any) => e.type === 'dom-mutation')
         ) {
+          const urlChange = msg.events?.find((e: any) => e.type === 'url-change');
           faro.api.pushEvent('faro.navigation', {
-            fromUrl: msg.events?.find((e: any) => e.type === 'url-change')?.from,
-            toUrl: msg.events?.find((e: any) => e.type === 'url-change')?.to,
-            sameDocument: 'true',
+            fromUrl: urlChange?.from,
+            toUrl: urlChange?.to,
+            sameDocument: String(true),
             duration: msg.duration,
           });
         }
