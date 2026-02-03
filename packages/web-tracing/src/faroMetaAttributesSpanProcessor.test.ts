8eb12df1b28845d0580a9eb51b99f19f205b63be
Iván Torres <torresgol10.it@gmail.com>
2026-02-03 12:08:09 +0100

    Add test multiple instances

--- packages/web-tracing/src/faroMetaAttributesSpanProcessor.test.ts
+++ packages/web-tracing/src/faroMetaAttributesSpanProcessor.test.ts
@@ -46,4 +46,65 @@ describe('faroMetaAttributesSpanProcessor', () => {
       'user.hash': 'hash',
     });
   });
+
+  it('support multiple instances having their own metas', () => {
+    const processor1 = new FaroMetaAttributesSpanProcessor(
+      {
+        onStart: jest.fn(),
+        onEnd: jest.fn(),
+        shutdown: jest.fn(),
+        forceFlush: jest.fn(),
+      },
+      {
+        value: {
+          session: {
+            id: 'session-1',
+          },
+        },
+        add: jest.fn(),
+        remove: jest.fn(),
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+      }
+    );
+
+    const processor2 = new FaroMetaAttributesSpanProcessor(
+      {
+        onStart: jest.fn(),
+        onEnd: jest.fn(),
+        shutdown: jest.fn(),
+        forceFlush: jest.fn(),
+      },
+      {
+        value: {
+          session: {
+            id: 'session-2',
+          },
+        },
+        add: jest.fn(),
+        remove: jest.fn(),
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+      }
+    );
+
+    const span1 = {
+      attributes: {},
+    };
+
+    const span2 = {
+      attributes: {},
+    };
+
+    processor1.onStart(span1 as any, {} as any);
+    processor2.onStart(span2 as any, {} as any);
+
+    expect(span1.attributes).toMatchObject({
+      'session.id': 'session-1',
+    });
+
+    expect(span2.attributes).toMatchObject({
+      'session.id': 'session-2',
+    });
+  });
 });
