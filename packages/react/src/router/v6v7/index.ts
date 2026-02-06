d3b9077ef45eba20913899f560a3fb1a029b8ea0
marco.schaefer <marco.schaefer@grafana.com>
2026-02-06 15:23:10 +0100

    add setReactRouterV7SSRDependencies

--- packages/react/src/router/v6v7/index.ts
+++ packages/react/src/router/v6v7/index.ts
@@ -9,7 +9,7 @@ export {
   createReactRouterV7Options,
 } from './initialize';
 
-export { setReactRouterV6SSRDependencies } from './routerDependencies';
+export { setReactRouterV6SSRDependencies, setReactRouterV7SSRDependencies } from './routerDependencies';
 
 export { withFaroRouterInstrumentation } from './withFaroRouterInstrumentation';
 
