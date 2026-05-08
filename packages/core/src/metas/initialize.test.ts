0fd4cdb355a1a6ff5d706db3dff0b957dba2d15b
Elliot Kirk <elliot@elliotkirk.com>
2026-05-07 17:09:00 -0700

    test(core): add tests for getGitHash and app.gitHash meta registration

--- packages/core/src/metas/initialize.test.ts
+++ packages/core/src/metas/initialize.test.ts
@@ -2,6 +2,22 @@ import { initializeFaro } from '../initialize';
 import { mockConfig } from '../testUtils';
 
 describe('metas', () => {
+  it('sets app.gitHash from global object on initialization', () => {
+    (global as any).__faroGitHash_test = 'abc123def456abc123def456abc123def456abc1';
+
+    const { metas } = initializeFaro(mockConfig());
+    expect(metas.value.app?.gitHash).toEqual('abc123def456abc123def456abc123def456abc1');
+
+    delete (global as any).__faroGitHash_test;
+  });
+
+  it('leaves app.gitHash undefined when global is not set', () => {
+    delete (global as any).__faroGitHash_test;
+
+    const { metas } = initializeFaro(mockConfig());
+    expect(metas.value.app?.gitHash).toBeUndefined();
+  });
+
   it('can set listeners and they will be notified on meta changes', () => {
     const { metas } = initializeFaro(mockConfig());
 
--- packages/core/src/utils/sourceMaps.test.ts
+++ packages/core/src/utils/sourceMaps.test.ts
@@ -1,12 +1,14 @@
-import { getBundleId } from './sourceMaps';
+import { getBundleId, getGitHash } from './sourceMaps';
 
 describe('sourceMapUpload utils', () => {
   beforeAll(() => {
     delete (global as any).__faroBundleId_foo;
+    delete (global as any).__faroGitHash_foo;
   });
 
   afterAll(() => {
     delete (global as any).__faroBundleId_foo;
+    delete (global as any).__faroGitHash_foo;
   });
 
   it('can get the bundle ID from the global object', () => {
@@ -14,5 +16,14 @@ describe('sourceMapUpload utils', () => {
 
     (global as any).__faroBundleId_foo = 'bar';
     expect(getBundleId('foo')).toEqual('bar');
+  });
+
+  it('returns undefined for git hash when not set', () => {
+    expect(getGitHash('foo')).toBeUndefined();
+  });
+
+  it('can get the git hash from the global object', () => {
+    (global as any).__faroGitHash_foo = 'abc123def456abc123def456abc123def456abc1';
+    expect(getGitHash('foo')).toEqual('abc123def456abc123def456abc123def456abc1');
   });
 });
