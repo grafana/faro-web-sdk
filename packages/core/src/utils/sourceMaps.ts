9c82ce49bc6d02c8a9b9a83227f101ce224a79b8
Elliot Kirk <elliot.kirk@grafana.com>
2026-05-19 15:50:15 -0700

    feat(core): include git commit hash in app meta (#2039)

--- packages/core/src/utils/sourceMaps.ts
+++ packages/core/src/utils/sourceMaps.ts
@@ -3,3 +3,7 @@ import { globalObject } from '../globalObject';
 export function getBundleId(appName: string): string | undefined {
   return (globalObject as any)?.[`__faroBundleId_${appName}`];
 }
+
+export function getGitHash(appName: string): string | undefined {
+  return (globalObject as any)?.[`__faroGitHash_${appName}`];
+}
