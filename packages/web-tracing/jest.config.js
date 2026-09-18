8e6cc445f731e42cde1ca3046fceadcff946e0b6
Simon Legander <simon.legander@gmail.com>
2026-09-18 20:11:30 +0200

    fix(tracing): activate prerender sessions before sampling

--- packages/web-tracing/jest.config.js
+++ packages/web-tracing/jest.config.js
@@ -2,6 +2,10 @@ const { jestBaseConfig } = require('../../jest.config.base.js');
 
 module.exports = {
   ...jestBaseConfig,
+  moduleNameMapper: {
+    ...jestBaseConfig.moduleNameMapper,
+    '@grafana/faro-web-sdk$': '<rootDir>/packages/web-sdk/src/index.ts',
+  },
   roots: ['packages/web-tracing/src'],
   testEnvironment: 'jsdom',
   setupFiles: ['<rootDir>/packages/web-tracing/setup.jest.ts'],
